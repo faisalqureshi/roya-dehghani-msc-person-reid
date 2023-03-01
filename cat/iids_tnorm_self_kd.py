13c7e1b7b0a1b6be118c348f54e43f9ac9b00138
royadeh <roya.dehghani@ontariotechu.net>
2023-03-01 01:29:35 -0500

    fix the path of current directory

--- cat/iids_tnorm_self_kd.py
+++ cat/iids_tnorm_self_kd.py
@@ -1,9 +1,12 @@
+
+
+
 from __future__ import print_function, absolute_import
+
 import argparse
-import os
 import os.path as osp
 import shutil
-
+import os
 import numpy as np
 import sys
 import torch
@@ -12,90 +15,91 @@ from torch.backends import cudnn
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 
-#add the path of your directory
-sys.path.append( os.path.abspath(os.path.join(os.getcwd(), '../iids')) )
+
+sys.path.append(os.getcwd())
 
 from reid.loss import TripletLoss, SoftEntropy, SoftTripletLoss
 from reid.loss.entropy_regularization import SoftEntropy
-
 from reid import datasets
 from reid import models
 from reid.trainers import IntraCameraSelfKDTnormTrainer
 from reid.trainers import InterCameraSelfKDTNormTrainer
 from reid.evaluators_cos import Evaluator
-
-#ina faghat 
 from reid.utils.data import transforms as T
 from reid.utils.data.preprocessor import Preprocessor
-from reid.utils.data.sampler import RandomIdentitySampler
-
 from reid.utils.logging import Logger
 from reid.utils.serialization import load_checkpoint, save_checkpoint
 from reid.cluster_utils import get_intra_cam_cluster_result
 from reid.cluster_utils import get_inter_cam_cluster_result_tnorm
+from reid.utils.data.sampler import RandomIdentitySampler
+
+import debugpy
 
 
+#debugpy.listen(5678)
+#print("debug")
+#debugpy.wait_for_client()
 
 
 
 def get_data(
-    name,
-    split_id,
-    data_dir,
-    height,
-    width,
-    batch_size,
-    workers,
-):
-    root = osp.join(data_dir, name)
+            name,
+            split_id,
+            data_dir,
+            height,
+            width,
+            batch_size,
+            workers,
+        ):
+            root = osp.join(data_dir, name)
 
-    dataset = datasets.create(name, root, split_id=split_id)
+            dataset = datasets.create(name, root, split_id=split_id)
 
-    normalizer = T.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    
-    train_set = dataset.trainval
-    num_classes = dataset.num_trainval_ids
+            normalizer = T.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+            
+            train_set = dataset.trainval
+            num_classes = dataset.num_trainval_ids
 
-    train_transformer = T.Compose([
-        T.Resize((height, width), interpolation=3),
-        T.ToTensor(),
-        normalizer,
-    ])
+            train_transformer = T.Compose([
+                T.Resize((height, width), interpolation=3),
+                T.ToTensor(),
+                normalizer,
+            ])
 
-    test_transformer = T.Compose([
-        T.Resize((height, width), interpolation=3),
-        T.ToTensor(),
-        normalizer,
-    ])
+            test_transformer = T.Compose([
+                T.Resize((height, width), interpolation=3),
+                T.ToTensor(),
+                normalizer,
+            ])
 
-    train_loader = DataLoader(Preprocessor(train_set,
-                                           root=dataset.images_dir,
-                                           transform=train_transformer),
-                              batch_size=batch_size,
-                              num_workers=workers,
-                              shuffle=False,
-                              pin_memory=False,
-                              drop_last=False)
+            train_loader = DataLoader(Preprocessor(train_set,
+                                                root=dataset.images_dir,
+                                                transform=train_transformer),
+                                    batch_size=batch_size,
+                                    num_workers=workers,
+                                    shuffle=False,
+                                    pin_memory=False,
+                                    drop_last=False)
 
-    val_loader = DataLoader(Preprocessor(dataset.val,
-                                         root=dataset.images_dir,
-                                         transform=test_transformer),
-                            batch_size=batch_size,
-                            num_workers=workers,
-                            shuffle=False,
-                            pin_memory=False)
+            val_loader = DataLoader(Preprocessor(dataset.val,
+                                                root=dataset.images_dir,
+                                                transform=test_transformer),
+                                    batch_size=batch_size,
+                                    num_workers=workers,
+                                    shuffle=False,
+                                    pin_memory=False)
 
-    test_loader = DataLoader(Preprocessor(
-        list(set(dataset.query) | set(dataset.gallery)),
-        root=dataset.images_dir,
-        transform=test_transformer),
-                             batch_size=batch_size,
-                             num_workers=workers,
-                             shuffle=False,
-                             pin_memory=False)
+            test_loader = DataLoader(Preprocessor(
+                list(set(dataset.query) | set(dataset.gallery)),
+                root=dataset.images_dir,
+                transform=test_transformer),
+                                    batch_size=batch_size,
+                                    num_workers=workers,
+                                    shuffle=False,
+                                    pin_memory=False)
 
-    return dataset, num_classes, train_loader, val_loader, test_loader
+            return dataset, num_classes, train_loader, val_loader, test_loader
 
 
 def make_params(model, lr, weight_decay):
@@ -119,11 +123,12 @@ def make_params(model, lr, weight_decay):
 def get_mix_rate(mix_rate, epoch, num_epoch, power=0.6):
     return mix_rate * (1 - epoch / num_epoch)**power
 
-#batch-size=8
-#batch_size_stage2=64
-#cluster_epochs=40
-#epoch_stage1=3
-#epoch_stage2=2
+    #batch-size=8
+    #batch_size_stage2=64
+    #cluster_epochs=40
+    #epoch_stage1=3
+    #epoch_stage2=2
+
 def main(args):
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
@@ -131,7 +136,7 @@ def main(args):
 
     # Redirect print to both console and log file
     sys.stdout = Logger(osp.join(args.logs_dir, 'log.txt'))
-    print(args)
+ 
     shutil.copy(sys.argv[0], osp.join(args.logs_dir,
                                       osp.basename(sys.argv[0])))
 
@@ -142,7 +147,7 @@ def main(args):
         get_data(args.dataset, args.split, args.data_dir, args.height,
                  args.width, args.batch_size * 8, args.workers,
                  )
-    camera_number = {"market1501": 6, "dukemtmc": 8, "msmt17": 15}
+    camera_number = {"market1501": 6, "dukemtmc": 8, "msmt17": 15, "viper": 2}
     
     # Create model
     #ft_net_inter_Tnorm=backbone+classifier
@@ -162,7 +167,7 @@ def main(args):
     top1 = 0
     is_best = False
     
-    #if we use pretrained model
+      #if we use pretrained model
     if args.checkpoint is not None:
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         
@@ -181,7 +186,7 @@ def main(args):
             model.model.load_param(args.checkpoint)
          
     model=model.to(device)
-    #model = model.cuda()
+      #model = model.cuda()
 
     # Distance metric
     metric = None
@@ -189,10 +194,10 @@ def main(args):
     # Evaluator
     
     evaluator = Evaluator(model, use_cpu=args.use_cpu)
-##################################################################  
+      ##################################################################  
     #if mode=evaluation
     if args.evaluate:
-        print("Test:")
+        
         evaluator.evaluate_tnorm(
             test_loader,
             dataset.query,
@@ -208,7 +213,7 @@ def main(args):
             metric,
         )
         return
-#############################################################################
+            #############################################################################
 
     train_transformer = [
         T.Resize((args.height, args.width), interpolation=3),
@@ -220,13 +225,13 @@ def main(args):
         T.RandomErasing(probability=0.5),
     ]
     train_transformer = T.Compose(train_transformer)
-#########################repeat 40 times for clustering 
-#and in each round, 3 times for training intra
-#and 2 times for training inter model
-    #cluster-epochs=40
+                #########################repeat 40 times for clustering 
+                #and in each round, 3 times for training intra
+                #and 2 times for training inter model
+                    #cluster-epochs=40
     for cluster_epoch in range(args.cluster_epochs):
-        # -------------------------Stage 1 intra camera training--------------------------
-        # Cluster and generate new dataset and model
+                    # -------------------------Stage 1 intra camera training--------------------------
+                    # Cluster and generate new dataset and model
         cluster_result = get_intra_cam_cluster_result(model, train_loader,
                                                       args.eph_stage1,
                                                       args.linkage)
@@ -257,7 +262,7 @@ def main(args):
             init_weight=args.init_weight)
        
         
-     
+       
         model_param_dict = model.model.state_dict()
         for k, v in model_param_dict.items():
             if k in param_dict.keys():
@@ -279,7 +284,7 @@ def main(args):
                                                 soft_criterion,
                                                 warm_up_epoch=args.warm_up,
                                                 multi_task_weight=args.multi_task_weight,)
-        print("start training")
+        
         # Start training
         #epochs_stage1=3
         for epoch in range(0, args.epochs_stage1):
@@ -357,7 +362,7 @@ def main(args):
             warm_up_epoch=args.warm_up,
         )
 
-        print("start training")
+        #print("start training")
         # Start training
         for epoch in range(0, args.epochs_stage2):
             trainer.train(cluster_epoch,
@@ -398,8 +403,8 @@ def main(args):
 
 
 
-    # Final test
-    print('Test with best model:')
+    print("Final test for evaluation trained model on test set")
+   
     checkpoint = load_checkpoint(osp.join(args.logs_dir, 'model_best.pth.tar'))
     new_state_dict = {}
     for k in checkpoint['state_dict'].keys():
@@ -495,11 +500,12 @@ if __name__ == '__main__':
     parser.add_argument('--data-dir',
                         type=str,
                         metavar='PATH',
-                        default=osp.join(working_dir, 'data'))
+                        #default=osp.join(working_dir, 'data')
+                        )
     parser.add_argument('--logs-dir',
                         type=str,
                         metavar='PATH',
-                        default=osp.join(working_dir, '../logs'))
+                        default=osp.join(working_dir, './logs'))
     
     main(parser.parse_args())
     torch.cuda.set_device(1)
